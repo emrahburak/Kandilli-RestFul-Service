c0159c4b17d72cf6fd48b937a18ec2293daa084c
emrahburak <gemrahburak@gmail.com>
2020-09-12 21:29:42 +0300

    api-lab-prod:1.0.4

--- Api-Kandilli/src/main/java/com/apikandilli/ApiKandilli/Entities/Kandilli.java
+++ Api-Kandilli/src/main/java/com/apikandilli/ApiKandilli/Entities/Kandilli.java
@@ -13,12 +13,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.xml.bind.annotation.XmlRootElement;
+
 
 /**
  *
